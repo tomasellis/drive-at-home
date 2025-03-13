c1b7d5bfbeb349931e407ccb6c3cd2f4a3b18d45
Tomas Ellis <tomas.sydney.ellis@gmail.com>
2025-03-13 05:56:26 -0300

    rendering db contents

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,29 +1,43 @@
-import { int, text, index, singlestoreTableCreator, bigint } from "drizzle-orm/singlestore-core";
+import "server-only";
 
-export const createTable = singlestoreTableCreator((name) => `drive-at-home_${name}`)
+import {
+  int,
+  text,
+  index,
+  singlestoreTableCreator,
+  bigint,
+} from "drizzle-orm/singlestore-core";
+
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-at-home_${name}`,
+);
 
 export const files = createTable(
-  "files_table", {
-  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
-  name: text("name"),
-  size: int("size"),
-  url: text("url"),
-  parent: bigint("parent", { mode: "number", unsigned: true }).notNull()
-}, (t) => {
-  return [
-    index("parent_index").on(t.parent)
-  ]
-},
-)
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name"),
+    size: int("size"),
+    url: text("url"),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
 
 export const folders = createTable(
-  "folders_table", {
-  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
-  name: text("name").notNull(),
-  parent: bigint("parent", { mode: "number", unsigned: true })
-}, (t) => {
-  return [
-    index("parent_index").on(t.parent)
-  ]
-},
-)
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
