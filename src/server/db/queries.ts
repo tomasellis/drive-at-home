2f75300dd5febb48c88335d03cec507bf11b3067
Tomas Ellis <tomas.sydney.ellis@gmail.com>
2025-03-14 04:54:43 -0300

    fix type errors

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -50,12 +50,13 @@ export const MUTATIONS = {
       name: string;
       size: number;
       url: string;
+      parent: number;
     };
     userId: string;
   }) {
     return await db.insert(filesSchema).values({
       ...input.file,
-      parent: 1,
+      parent: input.file.parent,
     });
   },
 };
